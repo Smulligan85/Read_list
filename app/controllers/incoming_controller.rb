e7ec30cd398b69a38724a711889d5d425c61eb49
Sean Mulligan <sean.mulligan85@gmail.com>
2015-04-13 07:07:52 -0700

    Incoming Controller Params

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,25 +8,25 @@ class IncomingController < ApplicationController
      # to get a sense of what you're dealing with.
      # puts "INCOMING PARAMS HERE: #{params}"
      
-    # # Find the user by using params[:sender]
-    @user = User.find(params[:sender])
-    # # Find the topic by using params[:subject]
-    @tag = Bookmark.find(@user.tag_id)
-    # # Assign the url to a variable after retreiving it from params["body-plain"]
-    @url = Bookmark.find(params["body-plain"])
+    # # # Find the user by using params[:sender]
+    # @user = User.find(params[:sender])
+    # # # Find the topic by using params[:subject]
+    # @tag = Bookmark.find(@user.tag_id)
+    # # # Assign the url to a variable after retreiving it from params["body-plain"]
+    # @url = Bookmark.find(params["body-plain"])
 
-    # # Check if user is nil, if so, create and save a new user
-    if @user == nil
-      @user.create.save
-    end
+    # # # Check if user is nil, if so, create and save a new user
+    # if @user == nil
+    #   @user.create.save
+    # end
 
-    # # Check if the topic is nil, if so, create and save a new topic
-    if @tag == nil
-      @tag.save
-    end
+    # # # Check if the topic is nil, if so, create and save a new topic
+    # if @tag == nil
+    #   @tag.save
+    # end
 
-    # # Now that you're sure you have a valid user and topic, build and save a new bookmark
-    @bookmark = Bookmark.build(bookmark_params)
+    # # # Now that you're sure you have a valid user and topic, build and save a new bookmark
+    # @bookmark = Bookmark.build(bookmark_params)
 
     # Assuming all went well. 
     head 200
