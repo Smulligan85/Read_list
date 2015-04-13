f22acc271a5fec4c72682a8a58b2495e443189bb
Sean Mulligan <sean.mulligan85@gmail.com>
2015-04-12 18:49:31 -0700

    Adding pundit authorization

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise'
 gem 'acts-as-taggable-on', '~> 3.4'
+gem 'pundit'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,23 +5,31 @@ class BookmarksController < ApplicationController
 
   def show
     @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
   end
 
   def new
     @bookmark = Bookmark.new
+    authorize @bookmark
   end
 
   def create
     @bookmark = Bookmark.create(bookmark_params)
-    @bookmark.save
-    redirect_to root_path
+    authorize @bookmark
+    if @bookmark.save
+      redirect_to root_path, notice: "Bookmark saved."
+    else
+      flash[:error] = "Bookmark could not be saved."
+      render :new
+    end
   end
 
   def update
-    @bookmark = Bookmark.find(params[:id])    
+    @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark   
     if @bookmark.update_attributes(bookmark_params)
       @bookmark.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Bookmark updated."
     else
       flash[:error] = "Could not update bookmark. Please try again."
     end
@@ -33,6 +41,7 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
     @bookmark.destroy
     redirect_to root_path
   end
