02b050b45211180ba033f9b68621e9092330c692
Sean Mulligan <sean.mulligan85@gmail.com>
2015-04-17 06:29:41 -0700

    Adding friendly id gem

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'devise'
 gem 'acts-as-taggable-on', '~> 3.4'
 gem 'pundit'
+gem 'friendly_id'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,10 @@
 class Bookmark < ActiveRecord::Base
+  extend FriendlyId
   belongs_to :user
   has_many :likes, dependent: :destroy
   acts_as_taggable
+
+  
+
+  friendly_id :url
 end
